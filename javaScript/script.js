1ecb857e1844fc6fe996b668b7d56a0227d3ba26
Steele Kreis <steele.kreis@gmail.com>
2021-10-03 00:07:19 -0600

    switching back to checkboxes, total js reset

--- javaScript/script.js
+++ javaScript/script.js
@@ -6,39 +6,17 @@ var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
 function writePassword() {
-  var password = null;
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
 
-};
+}
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
 
 
 function generatePassword() {
-
-
-
-
-
-
+  window.confirm("hello")
 };
-
-var myRange = document.querySelector('#myRange');
-var myValue = document.querySelector('#myValue');
-var myUnits = 'myUnits';
-var off = myRange.offsetWidth / (parseInt(myRange.max) - parseInt(myRange.min));
-var px =  ((myRange.valueAsNumber - parseInt(myRange.min)) * off) - (myValue.offsetParent.offsetWidth / 2);
-
-  myValue.parentElement.style.left = px + 'px';
-  myValue.parentElement.style.top = myRange.offsetHeight + 'px';
-  myValue.innerHTML = myRange.value + ' ' + myUnits;
-
-  myRange.oninput =function(){
-    let px = ((myRange.valueAsNumber - parseInt(myRange.min)) * off) - (myValue.offsetWidth / 2);
-    myValue.innerHTML = myRange.value + ' ' + myUnits;
-    myValue.parentElement.style.left = px + 'px';
-  };
