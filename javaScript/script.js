b4ae27ea1211e9765496fc6d25719a07d9951405
Steele Kreis <steele.kreis@gmail.com>
2021-10-03 15:41:09 -0600

    finishedgit add .! debugged pw length, added new section hiding feature

--- javaScript/script.js
+++ javaScript/script.js
@@ -1,11 +1,10 @@
-// Assignment code here
+// set constants and variables
 var empty = "";
 const lower = "abcdefghijklmnopqrstuvwxyz";
 const upper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 const numbers = "0123456789";
 const symbols = "!@#$%^&*_-+=";
 
-var length = document.getElementById("length").value;
 var incNumbers = document.getElementById("numbers");
 var incSymbols = document.getElementById("specChar");
 var incUppers = document.getElementById("uppers");
@@ -13,21 +12,35 @@ var incLowers = document.getElementById("lowers");
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
+var startBtn = document.querySelector("#start");
 
+//Hide the get started button and show everything else
+
+function showAndHide() {
+  document.getElementById("start").style.display = "none";
+  document.getElementById("criteria").style.display = "block";
+  document.getElementById("generate").style.display = "inline-block";
+};
 
 
 // Write password to the #password input
 function writePassword() {
+  var length = document.getElementById("num").value;
+  console.log(length);
+  var empty = "";
   incUppers.checked ? (empty += upper) : "";
   incNumbers.checked ? (empty += numbers) : "";
   incSymbols.checked ? (empty += symbols) : "";
   incLowers.checked ? (empty += lower) : "";
   var password = generatePassword(length, empty);
   var passwordText = document.querySelector("#password");
-
+  if (password == empty) {
+    window.alert("Please choose at least one character type.");
+    return;
+  };
   passwordText.value = password;
-
-}
+  document.getElementById("passwordCard").style.display = "block";
+};
 
 function generatePassword (length, empty) {
   let password = "";
@@ -41,6 +54,4 @@ function generatePassword (length, empty) {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
-
-
-console.log(password);
+startBtn.addEventListener("click", showAndHide);
