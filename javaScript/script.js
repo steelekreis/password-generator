d8c7fa5d8feab7d75d962734aa3aa645d5efa4b7
Steele Kreis <steele.kreis@gmail.com>
2021-10-03 01:09:10 -0600

    finally got the generator to workgit add .!

--- javaScript/script.js
+++ javaScript/script.js
@@ -1,31 +1,46 @@
 // Assignment code here
-const lower = "abcdefghijklmnopqrstuvwxyz"
-const upper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+var empty = "";
+const lower = "abcdefghijklmnopqrstuvwxyz";
+const upper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 const numbers = "0123456789";
 const symbols = "!@#$%^&*_-+=";
 
-const length = document.getElementById("length");
-const incNumbers = document.getElementById("nR");
-const incSymbols = document.getElementById("sR");
-const incNumbers = document.getElementById("uR");
-const incSymbols = document.getElementById("lR");
+var length = document.getElementById("length").value;
+var incNumbers = document.getElementById("numbers");
+var incSymbols = document.getElementById("specChar");
+var incUppers = document.getElementById("uppers");
+var incLowers = document.getElementById("lowers");
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
+
+
 // Write password to the #password input
 function writePassword() {
-  var password = generatePassword();
+  incUppers.checked ? (empty += upper) : "";
+  incNumbers.checked ? (empty += numbers) : "";
+  incSymbols.checked ? (empty += symbols) : "";
+  incLowers.checked ? (empty += lower) : "";
+  var password = generatePassword(length, empty);
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
 
 }
 
+function generatePassword (length, empty) {
+  let password = "";
+  for (let i = 0; i < length; i++) {
+    password += empty.charAt(
+      Math.floor(Math.random() * empty.length)
+    );
+  }
+  return password;
+};
+
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
 
 
-function generatePassword() {
-  window.confirm("hello")
-};
+console.log(password);
