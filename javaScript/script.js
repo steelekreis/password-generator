d47460f2a154f2a0c58da745c3db89fed25f6745
Steele Kreis <steele.kreis@gmail.com>
2021-10-03 00:13:29 -0600

    added const and var's to js

--- javaScript/script.js
+++ javaScript/script.js
@@ -1,5 +1,14 @@
 // Assignment code here
+const lower = "abcdefghijklmnopqrstuvwxyz"
+const upper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+const numbers = "0123456789";
+const symbols = "!@#$%^&*_-+=";
 
+const length = document.getElementById("length");
+const incNumbers = document.getElementById("nR");
+const incSymbols = document.getElementById("sR");
+const incNumbers = document.getElementById("uR");
+const incSymbols = document.getElementById("lR");
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
